b1f0acda7092f588a6726df408cb7c0b5fbc7c1b
Arsh-sharma <97118125+Arsh-sharma@users.noreply.github.com>
2022-01-10 16:41:55 +0530

    Add files via upload

---
+++ ASSIGNMENT1 OUTPUT .py
@@ -0,0 +1,26 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\ARSH SHARMA\Downloads\assignment1 input.py =========
+number one is: 1
+number two is: 3
+number three is: 5
+average is: 3.0
+what is your gross income? 50000
+how many members are in your family? 2
+income tax: 6800.0
+what is your name? ARSH SHARMA
+what is yor sid? 21103057
+what is your gender? F
+what is the name of your course? CSE
+what is your CGPA? 10.0
+details are: ('ARSH SHARMA', 21103057, 'F', 'CSE', 10.0)
+marks of student one: 17
+marks of student two: 14
+marks of student three: 19
+marks of student four: 21
+marks of student five: 20
+List before sorting: [17, 14, 19, 21, 20]
+list after sorting: [14, 17, 19, 20, 21]
+['red', 'green', 'white', 'pink', 'yellow']
+['red', 'green', 'white', 'purple', 'purple', 'yellow']
